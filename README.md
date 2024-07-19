b4ac2708536c97ce3465bb320bbfc7f7b4521316
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2024-07-19 10:32:17 -0400

    setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pine
+
 Pine Language
+
+## Design
+
+**Block**: A series of statements. Statements are seperated by line. The last expression of a block is returned as it's output. Each nested block gets an extended environment/scope.
+
+**Let Statement**: Assigns an identifier in environment/scope.
+
+**Type Statement**: Defines a form of data
+
+**Expression Statement**: Evaluates a standalone expression
+
+## Grammar
--- internal/token/token.go
+++ internal/token/token.go
@@ -1,1 +1,65 @@
 package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT  = "IDENT"  // add, foobar, x, y, ...
+	INT    = "INT"    // 1343456
+	STRING = "STRING" // "foobar"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA = ","
+	COLON = ":"
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	LET   = "LET"
+	TYPE  = "TYPE"
+	TRUE  = "TRUE"
+	FALSE = "FALSE"
+	IF    = "IF"
+	ELSE  = "ELSE"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"let":   LET,
+	"true":  TRUE,
+	"false": FALSE,
+	"if":    IF,
+	"else":  ELSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
